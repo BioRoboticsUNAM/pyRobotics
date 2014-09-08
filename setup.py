80be702777dd0423bdd0f6dbc355a1eef172b1f2
Adrián Revuelta Cuauhtli <adrianrc.89@gmail.com>
2014-09-08 14:52:31 -0500

    Generated new pypi distribution, renamed README.md to README.
    -> Modified description and version from setup.py, generated tar and uploaded to pypi.
    -> Modified README.md to README, hoping that both pypi and github would read it to display the project's info.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from distutils.core import setup
 
 setup(name='pyRobotics',
-    version='1.5',
+    version='1.5.1',
     author='Adrián Revuelta Cuauhtli',
     author_email='adrianrc.89@gmail.com',
     url='http://bioroboticsunam.github.io/pyRobotics',
     license='LICENSE.txt',
-    data_files=[('', ['README.md', 'LICENSE.txt'])],
-    description='A Python API to create modules that connect to our BlackBoard',
+    data_files=[('', ['README', 'LICENSE.txt'])],
+    description="A Python API to create modules that connect to our message-passing and shared varaibels hub 'BlackBoard'.",
     packages=['pyrobotics'])
