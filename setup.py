e26e13dc2d43589bbbdd1b53ad16446829a3aeba
Adrián Revuelta Cuauhtli <adrianrc.89@gmail.com>
2014-09-08 00:38:24 -0500

    Generated a source distribution with distutils package.
    -> Added setup file.
    -> Created the first distribution.
    -> Registered the library at pypi.

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='pyRobotics',
+    version='1.5',
+    author='Adrián Revuelta Cuauhtli',
+    author_email='adrianrc.89@gmail.com',
+    url='http://bioroboticsunam.github.io/pyRobotics',
+    license='LICENSE.txt',
+    packages=['pyrobotics'])
